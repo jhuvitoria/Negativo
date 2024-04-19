1689ef463aae6baa27412795c51b4c2475a1215f
jhu vitoria <jhuliavitoria226@gmail.com>
2024-04-19 15:01:30 -0300

    exercício concluido!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks.Sources;
+
+int numero;
+ 
+ Console.ForegroundColor = ConsoleColor.DarkMagenta;
+ Console.Write("Digite um número: ");
+ numero = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+if (numero < 0 )
+{
+Console.WriteLine("Voce digitou um numero negativo.");
+}
+Console.ResetColor();
+
